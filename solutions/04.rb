64d13f554b0700f4ddadd79d48ae9dc2d4de8765
root <root@ubuntu.(none)>
2014-01-22 03:08:11 +0200

    Give up all hope

--- solutions/04.rb
+++ solutions/04.rb
@@ -1,96 +1,114 @@
 module Asm
-  class Compiler
-    attr_reader :instructions, :labels
-
-    def initialize
-      @instructions = []
-      @labels = {}
+  class Mov < Struct.new :destination, :source
+    def execute(unit)
+      unit.registers[destination] = unit.get_value source
     end
+  end
 
-    MUTATORS = [:mov, :inc, :dec, :cmp].freeze
-    JUMPERS = {
-      jmp: proc { true },
-      je:  proc { @last_cmp.zero? },
-      jne: proc { not @last_cmp.zero? },
-      jl:  proc { @last_cmp < 0 },
-      jle: proc { @last_cmp <= 0 },
-      jg:  proc { @last_cmp > 0 },
-      jge: proc { @last_cmp >= 0 },
-    }.freeze
+  class Inc < Struct.new :destination, :value
+    def execute(unit)
+      unit.registers[destination] += value.nil? ? 1 : unit.get_value(value)
+    end
+  end
 
-    MUTATORS.each do |mutator_name|
-      define_method mutator_name do |*arguments|
-        @instructions << [mutator_name, *arguments]
+  class Dec < Struct.new :destination, :value
+    def execute(unit)
+      unit.registers[destination] -= value.nil? ? 1 : unit.get_value(value)
+    end
+  end
+
+  class Cmp < Struct.new :register, :value
+    def execute(unit)
+      unit.cmp_result = unit.registers[register] <=> unit.get_value(value)
+    end
+  end
+
+  class Jmp < Struct.new :where
+    def execute(unit)
+      unit.jump_to where
+    end
+  end
+
+  class Jump_Class
+  end
+
+  CONDITIONAL_JUMPS = {
+    je:  :==,
+    jne: :'!=',
+    jl:  :<,
+    jle: :<=,
+    jg:  :>,
+    jge: :>=,
+  }.freeze
+
+  def self.define_jump_instruction(instruction_name, comparator)
+    jump_class = Struct.new :where do
+      define_method :execute do |unit|
+        unit.jump_to where if unit.cmp_result.public_send comparator, 0
       end
     end
 
-    JUMPERS.each do |jumper_name, condition|
-      define_method jumper_name do |position|
-        @instructions << [:jmp, condition, position]
+    const_set instruction_name.capitalize, jump_class
+  end
+
+  CONDITIONAL_JUMPS.each do |class_name, comparator|
+    define_jump_instruction(class_name, comparator)
+  end
+
+  class Unit
+    attr_reader   :registers, :labels
+    attr_accessor :instructions, :cmp_result
+
+    def initialize(registers)
+      @registers           = Hash[registers.map { |name| [name.to_sym, 0] }]
+      @labels              = {}
+      @instructions        = []
+      @instruction_pointer = 0
+    end
+
+    def method_missing(name, *args)
+      if Instructions.const_defined?(name.capitalize)
+        instructions.push Instructions.const_get(name.capitalize).new *args
+      else
+        name
+      end
+    end
+
+    def get_value(value_or_register)
+      if value_or_register.is_a? Symbol
+        registers[value_or_register]
+      else
+        value_or_register
+      end
+    end
+
+    def jump_to(value_or_label)
+      if value_or_label.is_a? Symbol
+        @instruction_pointer = labels[value_or_label] - 1
+      else
+        @instruction_pointer = value_or_label - 1
       end
     end
 
     def label(name)
-      @labels[name] = @instructions.count
+      labels[name] = instructions.size
     end
 
-    def method_missing(label_or_register_name)
-      label_or_register_name
-    end
-
-    def self.compile(&program)
-      compiler = new
-      compiler.instance_eval &program
-
-      [compiler.instructions, compiler.labels]
-    end
-  end
-
-  class Executor < Struct.new(:instructions, :labels, :ax, :bx, :cx, :dx)
-    def mov(register, value)
-      self[register] = actual_value(value)
-    end
-
-    def inc(register, value = 1)
-      self[register] += actual_value(value)
-    end
-
-    def dec(register, value = 1)
-      self[register] -= actual_value(value)
-    end
-
-    def cmp(comparant_one, comparant_two)
-      @last_cmp = actual_value(comparant_one) <=> actual_value(comparant_two)
-    end
-
-    def jmp(condition, position)
-      @current_instruction = actual_position(position).pred if instance_eval &condition
-    end
-
-    def self.execute((instructions, labels))
-      new(instructions, labels, 0, 0, 0, 0).instance_eval do
-        @current_instruction = -1
-        while instructions[@current_instruction.next] do
-          @current_instruction += 1
-          send *instructions[@current_instruction]
-        end
-
-        [ax, bx, cx, dx]
+    def run
+      while @instruction_pointer.between?(0, instructions.size - 1)
+        instructions[@instruction_pointer].execute self
+        @instruction_pointer += 1
       end
-    end
 
-    private
-
-    def actual_value(value)
-      value.is_a?(Symbol) ? self[value] : value
-    end
-
-    def actual_position(position)
-      labels.fetch(position, position)
+      registers.values
     end
   end
 
-  def self.asm(&program)
-    Executor.execute(Compiler.compile &program)
+  def self.asm(&block)
+    unit = unit.new %w(ax bx cx dx)
+    unit.instance_eval(&block)
+
+    unit.run
   end
+
 end
